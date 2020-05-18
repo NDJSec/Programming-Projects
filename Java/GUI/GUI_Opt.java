7b89b2f7c1f6f9f9af23d123072ba5c4141a5a05
NDJSec <63249547+NDJSec@users.noreply.github.com>
2020-05-18 12:51:58 -0400

    Update GUI_Opt.java

--- Java/GUI/GUI_Opt.java
+++ Java/GUI/GUI_Opt.java
@@ -12,6 +12,7 @@ public class GUI_Opt extends JPanel implements ActionListener
     private JButton Euro;
     private JButton Pounds;
     private JButton CAD;
+    private JButton Clear;
     private JButton Exit;
     private double count = 0;
     private double raw = 0.0;
@@ -19,10 +20,10 @@ public class GUI_Opt extends JPanel implements ActionListener
 
     public GUI_Opt(JFrame frame)
     {
-        frame.setLayout(new GridLayout(0,1,15,40));
+        frame.setLayout(new GridLayout(0,1,15,0));
         
         JPanel f = new JPanel();
-        f.setLayout(new GridLayout(6,0,15,10));
+        f.setLayout(new GridLayout(5,2,15,5));
       
         CurrencyCounter = new JLabel("Currency Counter (In USD)", SwingConstants.CENTER);
         CurrencyCounter.setFont(new Font("Source Sans Pro", Font.BOLD, 15));  
@@ -32,27 +33,58 @@ public class GUI_Opt extends JPanel implements ActionListener
         f.add(tfCurrency);  
 
         USD = new JButton("USD");
+        USD.setOpaque(true);
+        USD.setBackground(Color.BLUE);
+        USD.setForeground(Color.WHITE);
         f.add(USD);                
         USD.addActionListener(this);
 
         Euro = new JButton("EUR");
+        Euro.setOpaque(true);
+        Euro.setBackground(Color.YELLOW);
+        Euro.setForeground(Color.BLACK);
         f.add(Euro);
         Euro.addActionListener(this);
 
         Pounds = new JButton("GBP");
+        Pounds.setOpaque(true);
+        Pounds.setBackground(Color.GREEN);
+        Pounds.setForeground(Color.BLACK);
         f.add(Pounds);
         Pounds.addActionListener(this);
 
         CAD = new JButton("CAD");
+        CAD.setOpaque(true);
+        CAD.setBackground(Color.RED);
+        CAD.setForeground(Color.WHITE);
         f.add(CAD);
         CAD.addActionListener(this);
 
-        Exit = new JButton("Exit");
-        f.add(Exit);
-        Exit.addActionListener(this);
-
-                    
         frame.add(f);
+
+        JPanel c = new JPanel();
+        c.setLayout(new FlowLayout());
+        Clear = new JButton("Clear");
+        Clear.setOpaque(true);
+        Clear.setBackground(Color.RED);
+        Clear.setForeground(Color.WHITE);
+        c.add(Clear);
+        Clear.addActionListener(this);
+        Clear.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        frame.add(c);
+
+        JPanel e = new JPanel();
+        e.setLayout(new FlowLayout());
+        Exit = new JButton("Exit");
+        Exit.setOpaque(true);
+        Exit.setBackground(Color.BLACK);
+        Exit.setForeground(Color.WHITE);
+        e.add(Exit);
+        Exit.addActionListener(this);
+        Exit.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        frame.add(e);
+
+
     }
 
     @Override
@@ -217,6 +249,11 @@ public class GUI_Opt extends JPanel implements ActionListener
 
                 tfCurrency.setText(count + "" + ":CAD");
 
+                break;
+
+            case "Clear":
+                count = 0;
+                tfCurrency.setText(count + "");
                 break;
             case "Exit":
                 System.exit(0);
